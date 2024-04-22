b1841d841f01fcf42c8f7fa1030c1c7f1689f0ed
Marcelo <mrcelo014@gmail.com>
2024-04-22 18:07:49 -0300

    doc

--- lib/app.rb
+++ lib/app.rb
@@ -38,16 +38,29 @@ class App
   end
 
   def call_method_from_class
+    # transforma em uma string e separa ela por "_",
+    # assim tranformando em um array, ex: "gatinho_fofo".split '_' -> ["gatinho", "fofo"].last -> "fofo"
     class_name = @menu.options[@user_option].to_s.split('_').last
+
+    # o mesmo da linha de cima, so que pegando o primeiro elemento do array
     class_action = @menu.options[@user_option].to_s.split('_').first
 
+    # chama a função responsavel por trata a string que vai ser o nome da class, de forma
+    # adequada, para que corresponda ao padrão
     class_name = format_class_name class_name
 
+    # retorna a class cujo o nome for especificado
     class_constant = Object.const_get class_name
+
+    # Então, class_constant.send(class_action) está chamando o método na
+    # classe representada por class_constant, onde o nome do método é
+    # especificado pela string class_action.
     class_constant.send class_action
   end
 
   def format_class_name(class_name)
+    # caso tenha um 's' no fim, ele sera removido e depois os resto da string sera convertida
+    # para letras caixa alta
     if class_name.chars.last == 's'
       class_name.slice(0, class_name.length - 1).capitalize
     else
